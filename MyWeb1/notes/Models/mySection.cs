1cd691326186a0e352e5a237e001e8fd23ff52c2
Eru Rorato <sadwolfy@gmail.com>
2016-04-20 22:31:40 +0400

    Section

---
+++ MyWeb1/notes/Models/mySection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace notes.Models
+{
+    public class mySection: System.Configuration.ConfigurationSection
+    {
+       public static mySection Get()
+       {
+           var widthSect = System.Configuration.ConfigurationManager.GetSection("nameMode") as mySection;
+           if (widthSect == null) { widthSect = new mySection(); }
+           return widthSect;
+       }
+       [System.Configuration.ConfigurationProperty("mode", IsRequired = false)]
+       public string mode => this["mode"] as string;
+
+    }
+}
